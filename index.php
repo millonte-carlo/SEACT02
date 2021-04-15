c9ceb8e588fffdd7a8d5adb342c21ffef550ed2a
carlomillonte <carlomillonte@gmail.com>
2021-04-15 12:15:13 +0800

    Version 1

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<title> SE - ACT01 - Changes </title>
+<body>
+<Center> Good Day Sir Crimgene </center>
+<table>
+    <tr>
+        <th><a href="phpsamplecode01.php">1</a></th>
+        <th><a href="phpsamplecode02.php">2</a></th>
+        <th><a href="phpsamplecode02.php">3</a></th>
+    </tr>
+</table>
+
+
+</body>
+</html>
---
+++ phpsamplecode02.php
@@ -0,0 +1,18 @@
+<html>
+<title> Loops </title>
+<head> <!--functions plcaed here--></head>
+<body>
+
+<?php include("index.php");
+?>
+
+<?php
+$x = 1;
+
+while($x <= 5) {
+  echo "The number is: $x <br>";
+  $x++;
+}
+?>
+</body>
+</html>
